d2e1d0d8da355a66f3593045e4d18d1946221034
geostojanov <gestojanov@gmail.com>
2023-05-09 10:25:00 +0200

    Pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"><Home/></Route>
+          <Route path="/create"><Create/></Route>
+          <Route path="/search"><Search/></Route>
+          <Route path="/recipes/:id"><Recipe/></Route>
+        </Switch>
+      </BrowserRouter>
 
     </div>
   );
