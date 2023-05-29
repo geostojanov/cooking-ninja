70ce3ef8f2c0f9d69a82e7ab2fa283682ea309da
george <gestojanov@gmail.com>
2023-05-29 09:18:40 +0200

    Reducers & useReducer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,11 @@ import {useTheme} from "../hooks/useTheme";
 
 export default function Navbar() {
 
-  const { color } = useTheme()
+  const { color, changeColor } = useTheme()
 
   return (
     <div className="navbar" style={{background: color}}>
-      <nav>
+      <nav onClick={() => changeColor('pink')}>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
